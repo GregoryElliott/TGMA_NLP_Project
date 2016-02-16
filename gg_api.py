be19d628ab4116fc8d8a72cb123746e657a701d6
GregoryElliott <gregoryelliott34@yahoo.com>
2016-02-16 15:30:57 -0600

    more updates

--- gg_api.py
+++ gg_api.py
@@ -189,60 +189,82 @@ def get_awards(year):
     #pn_count_vec = [["none", 0]]
     winners_old = []
     db = db2013
-    events = []
+    events = {}
+    event_freq = {}
 
     propers = []
+    
+    for event_index in get_event_indicies(get_tpm_arr((db))):
+        for n in range(500):
+            e = db[event_index + n]
+            if re.search(r'win(s)?(ner)?', e['text'], re.IGNORECASE) and re.search(r'best', e['text'], re.IGNORECASE):
+                for proper in strip_propers(normalize_str(e['text']).split()):
+                    propers.append(proper)
+                    events[event_index] = proper
 
-    for e in db:      
-        if re.search(r'win(s)?(ner)?', e['text'], re.IGNORECASE) and re.search(r'best', e['text'], re.IGNORECASE):
-            for proper in strip_propers(normalize_str(e['text']).split()):
-                propers.append(proper)
-
-    freq = FreqDist(propers)
-    events.append(freq)
+    for i in events:
+        freq = FreqDist(events[i])
+        event_freq[i] = freq
 
     winners_names = Counter()
-    for p in events[0].most_common(1000):
-        if "Best" not in p[0]:
-            continue
-        item_split = p[0].split(" ")
-        new_string = ''
-        start = False     
-        print item_split   
-        for cur in item_split:
-            print cur
-            if start:
-                if len(cur) == 0:
-                    continue
-                if not cur[0].isupper() or ',' in cur or '\'s' in cur or '.' in cur or 'buy' in cur:
-                    break
-                new_string = new_string +  ' ' + cur
-            if cur == "Best":
-                new_string = 'Best'
-                start = True            
-        if new_string != '':
-            if new_string not in winners_names.keys():
-                winners_names[new_string] = 0
-            winners_names[new_string] += p[1]
+    for i in event_freq:
+        for p in event_freq[i].most_common(15):
+            if "Best" not in p[0]:
+                continue
+            item_split = p[0].split(" ")
+            new_string = ''
+            start = False     
+            for cur in item_split:
+                print cur
+                if start:
+                    if len(cur) == 0:
+                        continue
+                    if '!' in cur:
+                        new_string = new_string +  ' ' + cur
+                        new_string.replace("!", "")
+                        break
+                    if ':' in cur:
+                        new_string = new_string +  ' ' + cur
+                        new_string.replace(":", "")
+                        break
+                    if not cur[0].isupper() or ',' in cur or '\'s' in cur or '.' in cur or 'buy' in cur:
+                        break
+                    new_string = new_string +  ' ' + cur
+                if cur == "Best":
+                    new_string = 'Best'
+                    start = True            
+            if new_string != '':
+                new_string.replace('!', '')
+                new_string.replace(':', '')
+                if (new_string, i) not in winners_names.keys():
+                    winners_names[(new_string, i)] = 0
+                winners_names[(new_string, i)] += p[1]
 
     for item in winners_names.most_common(50):
-        winners_old.append(item)
-        #stop = False
-        #remove = None
-        #for win in winners_old:
-            #if stop:
-                #break
+        if item[0][0] != "Best" and item[0][0] != "Best Buy":
+            winners_old.append(item[0])
+        else:
+            continue            
+        stop = False
+        remove = None
+        for win in winners_old:
+            print win
+            if stop:
+                break
             #for cur in item[0].split(" "):
                 #if "Best" in cur:
                     #continue
-                #if cur in win:
-                    #if len(item) > len(win):
-                        #winners_old.append(item[0])
-                        #remove = win
-                        #stop = True
-                        #break      
-        #if remove:
-            #winners_old.remove(remove) 
+            if (len(item[0][0].split(" ")) > 1 and item[0][0].split(" ")[1] == '') or (len(item[0][0].split(" ")) > 2 and item[0][0].split(" ")[2] == ''):
+                continue
+            if (len(item[0][0].split(" ")) > 2 and item[0][0].split(" ")[1] in win[0]) or (len(item[0][0].split(" ")) > 2 and item[0][0].split(" ")[2] in win[0]):
+                if len(item[0][0]) > len(win[0]):
+                    remove = win
+                    stop = True
+                    break      
+                else:
+                    remove = item[0]
+        if remove:
+            winners_old.remove(remove) 
 
 
         #winners_old.append(item[0])  
@@ -266,18 +288,59 @@ def get_nominees(year):
 
 def get_winners(year):
     #pn_count_vec = [["none", 0]]
+    #awards = get_awards(2013)
+    #awards = [u'best performance by an actress in a supporting role in a motion picture', u'best performance by an actor in a mini-series or motion picture made for television', u'best director - motion picture', u'best original song - motion picture', u'best performance by an actor in a motion picture - comedy or musical', u'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television', u'best performance by an actress in a motion picture - comedy or musical', u'cecil b. demille award', u'best performance by an actress in a mini-series or motion picture made for television', u'best animated feature film', u'best motion picture - drama', u'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', u'best performance by an actor in a television series - drama', u'best performance by an actress in a motion picture - drama', u'best screenplay - motion picture', u'best original score - motion picture', u'best foreign language film', u'best performance by an actor in a supporting role in a motion picture', u'best motion picture - comedy or musical', u'best mini-series or motion picture made for television', u'best television series - drama', u'best performance by an actress in a television series - comedy or musical', u'best television series - comedy or musical', u'best performance by an actor in a motion picture - drama', u'best performance by an actress in a television series - drama', u'best performance by an actor in a television series - comedy or musical']
+    awards = [u'best supporting actress', u'best actor mini-series television', u'best director', u'best original song', u'best actor', u'best supporting actor television', u'best actress', u'cecil b. demille award', u'best actress television', u'best animated feature film', u'best drama', u'best supporting actress television', u'best actor television drama', u'best actress drama', u'best screenplay', u'best original score', u'best foreign language film', u'best supporting actor', u'best motion picture comedy or musical', u'best mini-series or motion picture made for television', u'best television series drama', u'best actress television series comedy or musical', u'best television series comedy or musical', u'best actor drama', u'best actress drama', u'best actor comedy or musical']
     winners_old = []
     db = db2013
+    winners = {}
+    winners_list = {}
+    winners_freq = {}
     events = []
 
     propers = []
 
-    for e in db:      
-        if re.search(r'win(s)?(ner)?', e['text'], re.IGNORECASE) and re.search(r'best', e['text'], re.IGNORECASE):
-            for proper in strip_propers(normalize_str(e['text']).split()):
-                propers.append(proper)
+    # for e in db:    
+    #     if re.search(r'win(s)?(ner)?', e['text'], re.IGNORECASE) and re.search(r'best', e['text'], re.IGNORECASE):
+    #         for proper in strip_propers(normalize_str(e['text']).split()):
+    #             propers.append(proper)
 
-    freq = FreqDist(propers)
+
+    for award in awards:
+        winners_list[award] = []
+    print len(awards)
+    for award in awards: 
+        print award
+        for e in db:
+            if "RT" not in e['text']:
+                continue
+            count = 0
+            for a in award.split(" "):
+                if a in e['text']:
+                    count = count + 1
+                if count > 1:
+                    for proper in strip_propers(normalize_str(e['text']).split()):
+                        #propers.append(proper)
+                        p = proper.strip(' ')                        
+                        winners_list[award].append(p)
+                    continue
+
+    for a in winners_list:
+        freq = FreqDist(winners_list[a])
+        winners_freq[a] = freq
+    
+    for h in winners_freq:
+        print h
+        for win in winners_freq[h].most_common(20):
+            for a in h.split(' '):
+                if a in win:
+                    continue
+                if "RT" in win:
+                    continue
+                winners[h] = win
+                break
+    return winners
+
     events.append(freq)
 
     winners_names = Counter()
@@ -554,3 +617,95 @@ def get_winnersa(year):
                 print item
                 winner = winner + 1
     return winners
+
+
+def get_awardsa(year):
+    '''Awards is a list of strings. Do NOT change the name
+    of this function or what it returns.'''
+    # Your code here
+    #pn_count_vec = [["none", 0]]
+    winners_old = []
+    db = db2013
+    #events = {}
+    events = []
+
+    propers = []
+    
+    for e in db:
+        if re.search(r'win(s)?(ner)?', e['text'], re.IGNORECASE) and re.search(r'best', e['text'], re.IGNORECASE):
+            for proper in strip_propers(normalize_str(e['text']).split()):
+                propers.append(proper)
+
+    freq = FreqDist(propers)
+    events.append(freq)
+
+    winners_names = Counter()
+    for p in events[0].most_common(1000):
+        if "Best" not in p[0]:
+            continue
+        item_split = p[0].split(" ")
+        new_string = ''
+        start = False     
+        for cur in item_split:
+            print cur
+            if start:
+                if len(cur) == 0:
+                    continue
+                if '!' in cur:
+                    new_string = new_string +  ' ' + cur
+                    new_string.replace("!", "")
+                    break
+                if ':' in cur:
+                    new_string = new_string +  ' ' + cur
+                    new_string.replace(":", "")
+                    break
+                if not cur[0].isupper() or ',' in cur or '\'s' in cur or '.' in cur or 'buy' in cur:
+                    break
+                new_string = new_string +  ' ' + cur
+            if cur == "Best":
+                new_string = 'Best'
+                start = True            
+        if new_string != '':
+            new_string.replace('!', '')
+            new_string.replace(':', '')
+            if new_string not in winners_names.keys():
+                winners_names[new_string] = 0
+            winners_names[new_string] += p[1]
+
+    for item in winners_names.most_common(50):
+        if item[0] != "Best" and item[0] != "Best Buy":
+            winners_old.append(item[0])
+        else:
+            continue            
+        stop = False
+        remove = None
+        for win in winners_old:
+            print win
+            if stop:
+                break
+            #for cur in item[0].split(" "):
+                #if "Best" in cur:
+                    #continue
+            if (len(item[0].split(" ")) > 1 and item[0].split(" ")[1] == '') or (len(item[0].split(" ")) > 2 and item[0].split(" ")[2] == ''):
+                continue
+            if (len(item[0].split(" ")) > 2 and item[0].split(" ")[1] in win) or (len(item[0].split(" ")) > 2 and item[0].split(" ")[2] in win):
+                if len(item[0]) > len(win):
+                    remove = win
+                    stop = True
+                    break      
+                else:
+                    remove = item[0]
+        if remove:
+            winners_old.remove(remove) 
+
+
+        #winners_old.append(item[0])  
+    #winners = []          
+    #for winner in winners_old:
+        #winner_split = winner.split(' ')
+        #for win in winner_split:    
+            #if win not in winners
+
+    return winners_old
+
+    return awards
